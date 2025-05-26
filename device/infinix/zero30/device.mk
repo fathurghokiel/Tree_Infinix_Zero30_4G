2052b722f4a02ed4ddb6ef45d16f3604a1692505
fathurghokiel <fathurghokiel@gmail.com>
2025-05-26 17:39:42 +0700

    Update device fod.mk

--- device/infinix/zero30/device.mk
+++ device/infinix/zero30/device.mk
@@ -4,3 +4,5 @@ include device/transsion/mt6789-common/common.mk
 # Salin init file khusus device ini (jika ada)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.zero30.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.zero30.rc
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
