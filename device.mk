6d59fa28d470276cbbe77262a3b8a6845dcb2d18
fathurghokiel <fathurghokiel@gmail.com>
2025-05-26 13:23:51 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,6 @@
 
 # Inherit from mt6789-common
 $(call inherit-product, device/transsion/mt6789-common/common.mk)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service-mediatek.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-service-mediatek.rc
+
