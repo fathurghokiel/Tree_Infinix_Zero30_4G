a3729b85bc7a986c6bb5ceafd6d10ca6c69d5a2e
fathurghokiel <fathurghokiel@gmail.com>
2025-07-03 14:59:35 +0700

    Update AndroidProducts.mk
    Update

--- device/infinix/zero30/AndroidProducts.mk
+++ device/infinix/zero30/AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_zero30.mk
+    $(LOCAL_DIR)/Lineage_zero30.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zero30-user \
+    lineage_zero30-userdebug \
+    lineage_zero30-eng
