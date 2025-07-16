fc110e4a98988c12216c19d65a369b089345b7d0
fathurghokiel <fathurghokiel@gmail.com>
2025-07-16 16:23:58 +0700

    Update BoardConfig.mk

--- device/infinix/zero30/BoardConfig.mk
+++ device/infinix/zero30/BoardConfig.mk
@@ -1,13 +1,14 @@
+# Kernel sources
 TARGET_KERNEL_SOURCE := kernel/infinix/mt6789
 TARGET_KERNEL_CONFIG := zero30_4g_defconfig
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 
-# Inherit from common
-include device/infinix/mt6789_gen2-common/BoardConfigCommon.mk
-
-# FOD support jika ada fingerprint under display
+# FOD (Fingerprint On Display) support
 BOARD_HAS_FOD := true
 
-# Optional tambahan
+# Optional: Board name
 BOARD_NAME := zero30
+
+# Inherit from common BoardConfig
+include device/infinix/mt6789_gen2-common/BoardConfigCommon.mk
